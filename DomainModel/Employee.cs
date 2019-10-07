9937a4fd7bcdbca197f80b189e14392d642cfd33
Guillaume <petit.guillaume43@gmail.com>
2019-10-07 14:49:36 +0200

    Add Attribut for Client, Person and Employee

--- DomainModel/Employee.cs
+++ DomainModel/Employee.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace DomainModel
 {
     public class Employee : Person
     {
+        [NotNull]
         public string OfficeName { get; set; }
+        [NotNull]
         public bool IsJunior { get; set; }
         public Manager MyManager{ get; set; }
     }
--- DomainModel/Person.cs
+++ DomainModel/Person.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace DomainModel
 {
     public abstract class Person
     {
+        [Key]
+        public int PersonId { get; set; }
+        [NotNull]
         public string FisrtName { get; set; }
+        [NotNull]
         public string LastName { get; set; }
 
         [DataType(DataType.DateTime)]
