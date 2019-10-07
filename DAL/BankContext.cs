83e0b528d5cfe7fcdc79a0c90df2900ccd0c357b
Jerome <jerome.philipperon2@gmail.com>
2019-10-07 14:33:07 +0200

    Correction

---
+++ DAL/BankContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class BankContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; };
+    }
+}
