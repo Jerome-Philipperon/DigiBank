77ef570cec3df6123ee7bbd686cdab5cf91c8b3f
Guillaume <petit.guillaume43@gmail.com>
2019-10-07 14:30:27 +0200

    Add Person, Client, Employee and Manager Class

---
+++ DomainModel/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DomainModel
+{
+    public abstract class Person
+    {
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
