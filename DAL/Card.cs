d0b1e771beb6376aa933bc090e0dde176f58391e
mustapha <mustafa.belgacem@gmail.com>
2019-10-07 13:03:54 +0200

    class card

---
+++ DAL/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class Card
+    {
+
+   
+        public string NetworkIssuer { get; set; }
+        public string CardNumber{ get; set; }
+        public string SecurityCode { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+
+
+
+
+
+    }
+}
