0a4ca63e5be580af05dcaac48ed180219c8fbac4
Fabien <fabien.richelmy@gmail.com>
2019-10-09 10:36:27 +0200

    Attempt to fix Culture bug (commented in Startup.cs)

--- WebAppManagement/Startup.cs
+++ WebAppManagement/Startup.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json;
 using System.Linq;
 using System.Threading.Tasks;
 using DAL;
@@ -9,6 +11,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections;
+using Microsoft.AspNetCore.Localization;
 
 namespace WebAppManagement
 {
@@ -18,7 +22,7 @@ namespace WebAppManagement
         {
             Configuration = configuration;
         }
-
+            
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -26,11 +30,28 @@ namespace WebAppManagement
         {
             services.AddControllersWithViews();
             services.AddScoped<BankContext>();
+            // TENTATIVE DE RESOLUTION DU BUG CULTURE
+            //services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions. = CultureInfo.CurrentCulture);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // TENTATIVE DE RESOLUTION DU BUG CULTURE
+
+            //List<CultureInfo> supportedCultures = new List<CultureInfo>()
+            //{
+            //    new CultureInfo("en-US"),
+            //    new CultureInfo("fr-FR"),
+            //};
+
+            //app.UseRequestLocalization(new RequestLocalizationOptions
+            //{
+            //    DefaultRequestCulture = new RequestCulture("fr-FR"),
+            //    SupportedCultures = supportedCultures,
+            //    SupportedUICultures = supportedCultures
+            //});
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,6 +62,7 @@ namespace WebAppManagement
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+           
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
