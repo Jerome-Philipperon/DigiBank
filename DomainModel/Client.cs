de62a0915f87bf172c032669afcbaf36f003b116
mustapha <mustafa.belgacem@gmail.com>
2019-10-17 09:32:30 +0200

    web Api CLIENT

--- DomainModel/Client.cs
+++ DomainModel/Client.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace DomainModel
 {
     public class Client : Person
     {
+        [JsonPropertyName("Street")]
         [Required]
         public string Street { get; set; }
+        [JsonPropertyName("ZipCode")]
         [Required]
         public string ZipCode { get; set; }
         [Required]
+        [JsonPropertyName("City")]
         public string City { get; set; }
+        [JsonPropertyName("MyEmployee")]
         public Employee MyEmployee { get; set; }
-        
+        [JsonIgnore]
         public ICollection<Account> MyAccounts { get; set; }
         
     }
