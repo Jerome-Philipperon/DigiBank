3c5973320faac9743c8aa25b5986efd244cad7b8
Fabien <fabien.richelmy@gmail.com>
2019-10-18 18:13:27 +0200

    Link with Client informations (still a bug with Date)

--- AngularClient/src/app/Models/client.ts
+++ AngularClient/src/app/Models/client.ts
@@ -4,7 +4,7 @@ export default class Client {
     firstName : string;
     lastName : string;
     dateOfBirth : Date;
-    street : string
+    street : string;
     zipCode : number;
     city : string;
     myAccounts : Account[];
